0bbdb0d61ac73c086032b5421839bcb0ac0c10d9
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-18 19:57:49 -0700

    shortcut genesis

---
+++ project.clj
@@ -0,0 +1,7 @@
+
+(defproject shortcut "0.0.0"
+  :description "A graph (nodes and edges) library in clojure"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]                 
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+

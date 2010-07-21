3795a6ba480716890e75f4b3b9941172270e8ce4
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-21 09:46:36 -0700

    move make-graph to after add-edges defn and remove scratch stuff

--- src/shortcut/graph.clj
+++ src/shortcut/graph.clj
@@ -119,16 +119,16 @@
                           (remove-1-edge (:edge-map g) n2 n1)
                           n1 n2)))))
 
-(defn make-graph
-  ([] (Graph. #{} {}))
-  ([nodes] (Graph. nodes {}))
-  ([nodes edge-vec] (add-edges (Graph. nodes {}) edge-vec)))
-
 (defn add-edges [g edge-vec]
   (reduce (fn [g [n1 n2]] (add-edge g n1 n2)) g edge-vec))
 
 (defn add-nodes [g & node-vec]
   (reduce (fn [g node] (add-node g node)) g node-vec))
+
+(defn make-graph
+  ([] (Graph. #{} {}))
+  ([nodes] (Graph. nodes {}))
+  ([nodes edge-vec] (add-edges (Graph. nodes {}) edge-vec)))
 
 (defn breadth-first-traversal
   ([g start]
@@ -203,32 +203,4 @@
     (reduce remove-connected-component
             (remove-node g start)
             neighbors)))
-
-;;; scratch
-
-(def q (make-graph #{1 2 3 4} {}))
-(def q2 (add-edge (add-edge q 3 1) 1 4))
-(def q3 (add-edges q [[1 2] [1 3] [3 4]]))
-(def q4 (add-edges (add-nodes q3 5 6) [[4 5] [1 5] [4 6]]))
-(def q5 (add-edges (make-graph #{1 2 3 4 5 6})
-                   [[1 2] [2 3] [3 4] [4 5] [5 6] [6 1]]))
-
-(def q6 (reduce #(add-node %1 %2) (make-graph) (range 1000)))
-(def q7 (reduce (fn [g [n1 n2]] (add-edge g n1 n2))
-                q6
-                (take 10000 (repeatedly #(vector (rand-int 1000)
-                                                 (rand-int 1000))))))
-(neighbors q7 1)
-(take-while (complement #{2}) (breadth-first-traversal q7 1))
-
-(def q8 (reduce #(add-node %1 (str "node" %2)) (make-graph) (range 1000)))
-
-
-(def q9 (reduce (fn [g [n1 n2]] (add-edge g
-                                          (str "node" n1)
-                                          (str "node" n2)))
-                q8
-                (take 10000 (repeatedly #(vector (rand-int 1000)
-                                                 (rand-int 1000))))))
-
 

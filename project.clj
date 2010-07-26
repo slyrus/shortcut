88e1377667c6337a134be053e0aa54e14c6a1c9c
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-07-26 09:59:46 -0700

    now that shortcut works and has at least minimal documentation, bump to version 0.0.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject shortcut "0.0.0"
+(defproject shortcut "0.0.1"
   :description "A graph (nodes and edges) library in clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]                 

3d300481acfdaa950bb7531cabdcd4d414eb70be
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-08-01 22:41:03 -0700

    update to clojure-1.2,0-RC1 (and c.c.-1.2.0-RC1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 
 (defproject shortcut "0.0.1"
   :description "A graph (nodes and edges) library in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]                 
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 

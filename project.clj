9bb450eef07c0e7e76233b81810c5b501c6c55b5
Cyrus Harmon <ch-lisp@bobobeach.com>
2010-08-19 10:25:31 -0700

    update to clojure and clojure-contrib 1.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 
 (defproject shortcut "0.0.1"
   :description "A graph (nodes and edges) library in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
 
